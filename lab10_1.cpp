297750aea7f41edbb40416f01a1fe201985efdf8
Holytis <holytis123@gmail.com>
2022-12-27 15:25:27 +0700

    Yeah

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main(){	
+	double l , r , p, IT , TT, NB;
+	int i = 0 ;
 	cout << "Enter initial loan: ";
+	cin >> l ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> r ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> p ;
+	
+	
+	
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +28,29 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+
+	while(l > 0)
+	{
+		IT = l*r/100 ;
+		TT = l + IT ;
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i+1; 
+		cout << setw(13) << left << l;
+		cout << setw(13) << left << IT;
+		cout << setw(13) << left << TT;
+		if (TT <= p)
+		{
+			p = TT;
+			l = TT - TT;
+			cout << setw(13) << left << p;
+			cout << setw(13) << left << l;
+			break;
+		}
+		cout << setw(13) << left << p;
+		l = TT - p ;
+		cout << setw(13) << left << l;
+		cout << "\n";
+		i++;
+	}
 	return 0;
 }
